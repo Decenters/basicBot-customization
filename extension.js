06f6571d4314c854629373a3a4a572189a43ea59
Decenters <hfs77120@icloud.com>
2015-04-25 02:38:53 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 (function () {
     //Link location of your fork so you don't have to modify so many things.
-    var fork = "Yemasthui";
+    var fork = "Decenters";
 		
     //Define our function responsible for extending the bot.
     function extend() {
@@ -51,24 +51,7 @@
         bot.loadChat();
 
     }
-        bot.commands.meCommand = {
-            command: 'special',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.djLeave();
-                }
-            }
-        };
-
-        //Load the chat package again to account for any changes
-        bot.loadChat();
-
-    }
-
+   
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
