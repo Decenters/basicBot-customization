2e188f5fad0204b395f888af903a13b6e444045b
Decenters <hfs77120@icloud.com>
2015-04-26 18:05:27 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -93,7 +93,7 @@
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
         motdEnabled: true,
-        motdInterval: 10,
+        motdInterval: 3,
         motd: "!gif boobs",
         filterChat: true,
         etaRestriction: false,
